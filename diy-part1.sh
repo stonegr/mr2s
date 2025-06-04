32cdf215657aeda9486d0c5b444a8c1430d70cd2
stonegr <eloisasethxdg62@gmail.com>
2025-06-05 04:28:55 +0800

    cs

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,3 +6,6 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# add nf_deaf
+git clone https://github.com/stonegr/nf_deaf_op.git package/kernel/nf-deaf
