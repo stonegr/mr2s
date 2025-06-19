200c6e537d52a94e16e7c01fb218c8225851d9b4
stonegr <eloisasethxdg62@gmail.com>
2025-06-19 21:31:12 +0800

    fix config_generate

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.180/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.180/g' package/base-files/luci2/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/Stone/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/Stone/g' package/base-files/luci2/bin/config_generate
 
 # openssl enable asm
 sed -i '/OPENSSL_TARGET:=linux-$(call qstrip,$(CONFIG_ARCH))-openwrt/i \
